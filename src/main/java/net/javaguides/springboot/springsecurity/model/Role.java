c333b0356e7ee3c424293ed974d4ae5183fa8772
uhaseeb85 <uhaseeb85@gmail.com>
2021-06-27 21:33:34 -0700

    initial commit

---
+++ src/main/java/net/javaguides/springboot/springsecurity/model/Role.java
@@ -0,0 +1,68 @@
+package net.javaguides.springboot.springsecurity.model;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="role", schema = "public")
+public class Role {
+
+	/**
+	 * @param id
+	 * @param name
+	 */
+	public Role(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	@Id
+	@Column(name = "id", nullable = false)
+    private String id;
+	
+	@Column(name = "name", nullable = false)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+    	this.id = UUID.randomUUID().toString();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+}
---
+++ src/main/java/net/javaguides/springboot/springsecurity/model/User.java
@@ -0,0 +1,132 @@
+package net.javaguides.springboot.springsecurity.model;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name="user", schema = "public")
+public class User {
+
+	/**
+	 * @param id
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @param password
+	 * @param roles
+	 */
+	public User(String id, String firstName, String lastName, String email, String password, Collection<Role> roles) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	@Id
+	@Column(name = "id", nullable = false)
+    private String id;
+	@Column(name = "first_name", nullable = true)
+    private String firstName;
+	@Column(name = "last_name", nullable = true)
+    private String lastName;
+	@Column(name = "email", nullable = true)
+    private String email;
+	@Column(name = "password", nullable = true)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String firstName, String lastName, String email, String password, Collection<Role> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + "*********" + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+}
